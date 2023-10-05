7922d644146070617baaf696256d6f731ebfb9d7
Alessia Contenti <aco@unik.dk>
2023-10-05 11:58:23 +0200

    Init

---
+++ MessageFilters/Consumer/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var EXCHANGE_NAME = "quote";
+
+var factory = new ConnectionFactory() { HostName = "localhost", VirtualHost = "ucl"};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+channel.ExchangeDeclare(EXCHANGE_NAME, ExchangeType.Direct);
+
+FilteredReceive(channel, "Widget", EXCHANGE_NAME);
+FilteredReceive(channel, "Gadget", EXCHANGE_NAME);
+
+Console.WriteLine("Press [Enter] to exit.");
+Console.ReadLine();
+
+static void FilteredReceive(IModel channel, string filter, string exchangeName)
+{
+  Console.WriteLine($"{filter} waiting for messages. To exit press CTRL+C");
+
+  var queueName = channel.QueueDeclare().QueueName;
+  channel.QueueBind(queueName, exchangeName, filter);
+
+  var consumer = new EventingBasicConsumer(channel);
+  consumer.Received += (model, ea) =>
+  {
+    var body = ea.Body.ToArray();
+    var message = System.Text.Encoding.UTF8.GetString(body);
+    Console.WriteLine($"{filter} received message: '{message}'");
+  };
+
+  channel.BasicConsume(queueName, autoAck: true, consumer: consumer);
+}
---
+++ MessageFilters/Producer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var QUEUE_NAME = "eipqueue";
+var EXCHANGE_NAME = "quote";
+
+var factory = new ConnectionFactory() { HostName = "localhost", VirtualHost = "ucl"};
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+  channel.ExchangeDeclare(EXCHANGE_NAME, ExchangeType.Direct);
+
+  SendMessage(channel, EXCHANGE_NAME, "Widget", "Hello EIP Widget!");
+  SendMessage(channel, EXCHANGE_NAME, "Gadget", "Hello EIP Gadget!");
+}
+
+static void SendMessage(IModel channel, string exchange, string key, string message)
+{
+  var body = Encoding.UTF8.GetBytes(message);
+  channel.BasicPublish(exchange, key, null, body);
+  Console.WriteLine($" [x] Sent '{message}'");
+}
